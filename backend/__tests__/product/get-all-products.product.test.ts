2df34176b419494f3bfa2c28bf40c1e277d91263
Yunerkis Prevot <yprevot@gmail.com>
2024-02-18 12:21:18 -0600

    removing console.log from tests

--- backend/__tests__/product/get-all-products.product.test.ts
+++ backend/__tests__/product/get-all-products.product.test.ts
@@ -4,13 +4,11 @@ import { ExpressServer } from "../../src/ExpressServer";
 
 describe('Listing all products', ()=>{
     it('Empty results', async () => {
-        const result = await request(ExpressServer.getServer()).get('/api/v1/products');
-        console.log(result);
+        const result = await request(ExpressServer.getServer()).get('/api/v1/products');        
         expect(result.statusCode).toEqual(200);
     });
     it('Result with data', async () => {
         const result = await request(ExpressServer.getServer()).get('/api/v1/products');
-        console.log(result);
         expect(result.statusCode).toEqual(200);
     });
 });
--- backend/__tests__/server.test.ts
+++ backend/__tests__/server.test.ts
@@ -5,7 +5,6 @@ import { ExpressServer } from "../src/ExpressServer";
 describe('Server test', ()=>{    
    it('Server is working', async () => {
             const result = await request(ExpressServer.getServer()).get('/api/v1/products');
-            console.log(result);
             expect(result.statusCode).toEqual(200);
     });
 });
