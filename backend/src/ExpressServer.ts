061555c9b65226c4e44e5d66d9aaf60cbebcb363
Yunerkis Prevot <yprevot@gmail.com>
2024-02-22 11:41:53 -0600

    adding api tests

--- backend/src/ExpressServer.ts
+++ backend/src/ExpressServer.ts
@@ -1,14 +1,21 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { router } from './routes';
+import { HttpStatusCode } from './utils/HttpStatusCode';
 
 const app: Express = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.get('/', (request: Request, response: Response) => {
+    response.status(HttpStatusCode.OK).json({
+        message: 'API ready',
+    });
+});
 app.use('/api/v1', router);
 
 export { app as ExpressServer };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import { productsRouter } from './products.routes';
+import { HttpStatusCode } from '../utils/HttpStatusCode';
 
 const router = Router();
+router.get('/', (request: Request, response: Response) => {
+    response.status(HttpStatusCode.OK).json({
+        message: 'API Version 1.0',
+    });
+});
 router.use('/products', productsRouter);
 
 export { router };
