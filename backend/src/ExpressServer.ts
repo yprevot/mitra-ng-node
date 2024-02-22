3ef2c2d17f65dd06bbfba9105789407e6ea52d38
Prevot <yprevot@gmail.com>
2024-02-21 18:09:18 -0600

    Adding error middleware

--- backend/src/ExpressServer.ts
+++ backend/src/ExpressServer.ts
@@ -1,8 +1,10 @@
-import express, { Express } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { router } from './routes';
+import { HttpStatusCode } from './utils/HttpStatusCode';
+import { CustomError } from './utils/errors/errors.error';
 
 class ExpressServer {
     private static app: Express;
@@ -18,6 +20,24 @@ class ExpressServer {
                 ExpressServer.app.use(morgan('dev'));
                 ExpressServer.app.use(express.json());
                 ExpressServer.app.use('/api/v1', router);
+
+                ExpressServer.app.use('*', (request: Request, response: Response) => {
+                    response.status(HttpStatusCode.NOT_FOUND).json({
+                        message: `Can't find ${request.originalUrl} this route`,
+                    });
+                });
+                ExpressServer.app.use(
+                    (error: CustomError, request: Request, response: Response, next: NextFunction) => {
+                        error.status = error.status || 'fail';
+                        error.statusCode = error.statusCode || HttpStatusCode.INTERNAL_SERVER;
+                        response.status(error.statusCode).json({
+                            status: error.status,
+                            message: error.message,
+                            stack: error.stack,
+                            request: request.body
+                        });
+                    },
+                );
             } catch (error) {
                 console.error(error);
                 throw error;
---
+++ backend/src/utils/errors/errors.error.ts
@@ -0,0 +1,15 @@
+class CustomError extends Error {
+    public  statusCode: number = 0;
+    public status: string = '';
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = statusCode < 500 ? 'error' : 'fail';
+        Error.captureStackTrace(this, this.constructor);
+    }
+   
+}
+
+export {
+    CustomError
+}
